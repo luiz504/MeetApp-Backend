f398dc4d1fcd51b2de0a77e0b561d826643bad15
Luiz504 <luiz504@hotmail.com>
2019-10-28 09:12:00 -0300

    chore: Subscrition/migration/model

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Meetup from '../app/models/Meetup';
+import Subscrition from '../app/models/Subscription';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File, Meetup];
+const models = [User, File, Meetup, Subscrition];
 
 class Database {
   constructor() {
